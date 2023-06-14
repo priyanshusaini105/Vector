3cfa05ff50a64e45148f3e3e7b145afd72547ca9
Piyanshu Saini <priyanshusaininew@gmail.com>
2023-06-14 16:31:21 +0530

    Fix: player movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float forwardSpeed = 5f;
+    [SerializeField] private float jumpForce = 5f;
+
     private Rigidbody2D _player;
 
     private void Start()
@@ -10,9 +12,21 @@ public class PlayerController : MonoBehaviour
         _player = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Jump();
+        }
+    }
+
     private void FixedUpdate()
     {        
-        Debug.Log("i am mov");
         _player.velocity = new Vector2(forwardSpeed,0f);
+    }
+
+    private void Jump(){
+        Debug.Log("i am jumping");
+        _player.AddForce(Vector2.up *jumpForce, ForceMode2D.Impulse);
     }
 }
