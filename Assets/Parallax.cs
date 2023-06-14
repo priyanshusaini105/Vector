a2ab66fcf7d673168f38b702bc87d64efa580ef6
Piyanshu Saini <priyanshusaininew@gmail.com>
2023-06-15 02:29:17 +0530

    Add parallax effect

--- Assets/Parallax.cs
+++ Assets/Parallax.cs
@@ -8,16 +8,28 @@ public class Parallax : MonoBehaviour
     private Transform cameraTransform;
     private Vector3 lastCameraPosition;
 
-    private void Start() {
+    [SerializeField] private Transform characterTransform; // Reference to the character's transform
+
+    private void Start()
+    {
         cameraTransform = Camera.main.transform;
         lastCameraPosition = cameraTransform.position;
     }
 
-    private void LateUpdate() {
+    private void LateUpdate()
+    {
         Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
-        float parallaxEffectMultiplier = .5f;
-        transform.position += deltaMovement * parallaxEffectMultiplier;
+
+        // Only apply parallax effect in the X direction
+        Vector3 parallaxMovement = new Vector3(deltaMovement.x, 0f, 0f);
+
+        transform.position += parallaxMovement * parallaxEffectMultiplier;
+
         lastCameraPosition = cameraTransform.position;
 
+        // Make the parallax object follow the character's position in the Y direction
+        Vector3 newPosition = transform.position;
+        newPosition.y = characterTransform.position.y;
+        transform.position = newPosition;
     }
 }
