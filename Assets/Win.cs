f463d5dae7eaf3b14fec65cc742f95dd7f3bb1ba
Piyanshu Saini <priyanshusaininew@gmail.com>
2023-06-15 23:55:09 +0530

    Add collider to all buildings and change texture of the wall

---
+++ Assets/Win.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour
+{
+     private Rigidbody2D player;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            EndGame();
+        }
+    }
+
+    
+
+    void Log(string msg)
+    {
+        Debug.Log(msg);
+    }
+
+    void EndGame()
+    {
+        SceneManager.LoadScene("Win");
+    }
+}
