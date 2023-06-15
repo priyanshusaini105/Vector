7df094afcd5272e88ba33f6a3d2553eb682f674e
Piyanshu Saini <priyanshusaininew@gmail.com>
2023-06-15 23:15:49 +0530

    Game over on falling on the ground

--- Assets/Ground.cs
+++ Assets/Ground.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Ground : MonoBehaviour
 {
-    // Start is called before the first frame update
+     private Rigidbody2D player;
+
     void Start()
     {
-        
+        player = GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            EndGame();
+        }
+    }
+
+    
+
+    void Log(string msg)
+    {
+        Debug.Log(msg);
+    }
+
+    void EndGame()
+    {
+        SceneManager.LoadScene("EndGame");
     }
 }
