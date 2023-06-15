d611abb5c3065e884edbc800d478b5a6c042048a
Piyanshu Saini <priyanshusaininew@gmail.com>
2023-06-16 00:33:05 +0530

    Merge from jump-scene

---
+++ Assets/CollisionController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionController : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Building")
+        {
+            Debug.Log("enter");
+        }
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+         if (collision.gameObject.name == "Building")
+        {
+            Debug.Log("stay");
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+         if (collision.gameObject.name == "Building")
+        {
+            Debug.Log("exit");
+        }
+    }
+}
